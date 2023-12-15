bd5cfe7871fc09df6ac83269715493d8e27fc782
Eshwar <reshwar2004@gmail.com>
2023-12-15 17:37:18 +0530

    added exit button in leaderboard scene

---
+++ Assets/Scripts/HighscoreUI.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class HighscoreUI : MonoBehaviour
+{
+    public void loadMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
