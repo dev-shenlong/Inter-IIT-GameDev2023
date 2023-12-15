9c3c276e98d5fa4b3c6c6e05a96d402302dc809c
Eshwar <reshwar2004@gmail.com>
2023-12-15 19:27:39 +0530

    added help scene

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -24,4 +24,9 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadScene("leaderboard");
     }
+
+    public void LoadHelp()
+    {
+        SceneManager.LoadScene("help_scene");
+    }
 }
