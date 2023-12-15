2c19060ddfb1aa1afdca9a6a5f051dffab861b05
Eshwar <reshwar2004@gmail.com>
2023-12-15 17:56:51 +0530

    added score text ui and made it update accordingly

--- Assets/Scripts/itemcollectable.cs
+++ Assets/Scripts/itemcollectable.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class itemcollectable : MonoBehaviour
 {
+    [SerializeField] private TMP_Text scoreText;
     private int collectItem=0;  //Collected items here(Give point say 10)
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -12,6 +14,7 @@ public class itemcollectable : MonoBehaviour
             Destroy(collision.gameObject);
             collectItem += 30;
             Debug.Log("score: " + collectItem);
+            scoreText.text = "SCORE: " + collectItem.ToString();
         }
 
 
@@ -20,6 +23,7 @@ public class itemcollectable : MonoBehaviour
             Destroy(collision.gameObject);
             collectItem += 60;
             Debug.Log("score: " + collectItem);
+            scoreText.text = "SCORE: " + collectItem.ToString();
         }
 
         else if (collision.gameObject.CompareTag("collect3"))
@@ -27,6 +31,7 @@ public class itemcollectable : MonoBehaviour
             Destroy(collision.gameObject);
             collectItem +=100;
             Debug.Log("score: " + collectItem);
+            scoreText.text = "SCORE: " + collectItem.ToString();
         }
 
 
